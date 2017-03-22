c2f1e967c4dfe65f1a28c5a9b18aeba29ff1bd86
Tim Boudreau <timothy.boudreau@oracle.com>
2017-03-22 03:09:42 -0400

    Increase delay before connecting to mongodb to avoid crash

--- acteur-mongo/src/test/java/com/mastfrog/acteur/mongo/MongoHarness.java
+++ acteur-mongo/src/test/java/com/mastfrog/acteur/mongo/MongoHarness.java
@@ -33,6 +33,11 @@ public class MongoHarness {
     private final int port;
     private final Init mongo;
     private static int count = 1;
+    
+    /*
+    Try to connect too soon and you get a crash: https://jira.mongodb.org/browse/SERVER-23441
+    */
+    private static final int CONNECT_WAIT_MILLIS = 500;
 
     @Inject
     MongoHarness(@Named("mongoPort") int port, Init mongo) throws IOException, InterruptedException {
@@ -139,7 +144,13 @@ public class MongoHarness {
 
         private File createMongoDir() {
             File tmp = new File(System.getProperty("java.io.tmpdir"));
-            File mongoDir = new File(tmp, "mongo-" + System.currentTimeMillis() + "-" + count++);
+            String fname = System.getProperty("testMethodQname");
+            if (fname == null) {
+                fname = "mongo-" + System.currentTimeMillis() + "-" + count++;
+            } else {
+                fname += "-" + System.currentTimeMillis() + "-" + count++;
+            }
+            File mongoDir = new File(tmp, fname);
             if (!mongoDir.mkdirs()) {
                 throw new AssertionError("Could not create " + mongoDir);
             }
@@ -167,7 +178,7 @@ public class MongoHarness {
 
             // XXX instead of sleep, loop trying to connect?
             Process result = pb.start();
-            Thread.sleep(150);
+            Thread.sleep(CONNECT_WAIT_MILLIS);
             for (int i = 0;; i++) {
                 try {
                     Socket s = new Socket("localhost", port);
